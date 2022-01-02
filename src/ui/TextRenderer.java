da2be1c26ebb59f62f728503fe987fa5e8b71dcb
ootisg <ootisg@illcomics.com>
2022-01-02 03:32:51 -0600

    Added text line parsing

--- src/ui/TextRenderer.java
+++ src/ui/TextRenderer.java
@@ -1,28 +1,69 @@
 package ui;
 
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
 import json.JSONArray;
 import json.JSONObject;
+import main.GameAPI;
+import main.GameObject;
 
-public class TextRenderer {
+public class TextRenderer extends GameAPI {
 	
 	private JSONArray contents;
 	
 	public TextRenderer (JSONObject obj) {
 		
 		//Set text contents to the given JSON
-		contents = obj.getJSONArray ("textContents");
+		//contents = obj.getJSONArray ("textContents");
 		
 	}
 	
-	public void renderSubText (int x, int y, int boundWidth, int boundHeight, int startIndex, int endIndex) {
+	public void renderSubText (int x, int y, Rectangle bounds, int startIndex, int endIndex) {
 		
 	}
 	
-	public void renderText (int x, int y, int boundWidth, int boundHeight) {
+	public int renderText (int x, int y, Rectangle bounds) {
+		
+		return renderString (x, y, bounds, null, "123456789012345678901234567890123456789 123456789012345678901234567890123456789 A");
 		
 	}
 	
-	private void renderString (JSONObject context, String str) {
+	private int renderString (int x, int y, Rectangle bounds, JSONObject context, String str) {
+		
+		//# of padding pixels
+		int PADDING = 2;
+		
+		//Get the graphics object
+		Graphics g = getWindow ().getBufferGraphics ();
+		
+		//Set the color appropriately
+		g.setColor (Color.WHITE);
+		
+		//Draw the text
+		FontMetrics fm = g.getFontMetrics ();
+		int maxLen = bounds.x + bounds.width - x; //Line length
+		char[] buffer = str.toCharArray (); //Use a char[] for efficiency
+		int len = 0; //Line length
+		int offs = 0; //Line offset
+		int wy = y + fm.getAscent (); //Y to draw line
+		int wx = x; //X to draw line (wraps to start of bounds, so first line is indented to x)
+		while (wy < bounds.y + bounds.height && offs + len < buffer.length) {
+			while (fm.charsWidth (buffer, offs, len) < maxLen && offs + len < buffer.length) {
+				len++; //Add chars until out of data or the line length is exceeded
+			}
+			if (len == 0) {
+				return -1; //Prevent infinite loop edge case
+			}
+			g.drawChars (buffer, offs, len, x, wy); //Draws the line
+			wy += fm.getAscent () + PADDING; //Move wy down for the next line
+			wx = bounds.x; //Reset wx to the left of bounds
+			offs += len; //Advance the line offset in the buffer
+			len = 0; //Reset the string length
+		}
+		return offs; //Return the index of the last character drawn
 		
 	}
 
--- src/ui/Textbox.java
+++ src/ui/Textbox.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 import main.GameObject;
 import resources.Sprite;
@@ -20,11 +21,17 @@ public class Textbox extends GameObject {
 	private String emoteId;
 	private Sprite emoteImg;
 	
+	private TextRenderer textRenderer;
+	
 	public Textbox () {
 		
+		//Initialize bg and emote display
 		setSprite (textboxBg);
 		declare (BOX_OFFSET_H, getWindow ().getResolution ()[1] - textboxBg.getHeight () - BOX_OFFSET_V);
 		setEmote ("face_neutral");
+		
+		//Initialize the text renderer
+		textRenderer = new TextRenderer (null);
 		
 	}
 	
@@ -41,15 +48,16 @@ public class Textbox extends GameObject {
 	public void draw () {
 		
 		//Cast x and y to integers for convenience
-		int textX = (int)getX ();
-		int textY = (int)getY ();
+		int textX = (int)getX () + TEXT_OFFSET_X;
+		int textY = (int)getY () + TEXT_OFFSET_Y;
 		
 		//Draw the textbox and the background
 		super.draw ();
 		emoteImg.draw ((int)getX () + EMOTE_OFFSET, (int)getY () + EMOTE_OFFSET);
 		
 		//Draw the text
-		Graphics g = getWindow ().getBufferGraphics ();
+		Rectangle bounds = new Rectangle (textX, textY, 200, 40);
+		textRenderer.renderText (textX, textY, bounds);
 		
 	}
 
